bc38cc305bc1123828ad8e0440f1cd4cb0a6136c
Ameya G Mahale <ameya.mahale@iifl.com>
2023-06-07 14:49:11 +0530

    SmithShoes UI Changes

--- lib/Assignment/SmithShoes.dart
+++ lib/Assignment/SmithShoes.dart
@@ -49,123 +49,132 @@ class _SmithShoesState extends State<SmithShoes> {
         child: Container(
             width: double.infinity,
             height: double.infinity,
-            child: Column(
-              children: [
-                Image.network(
-                    'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1c2dfd0c-cd48-46c4-829a-4b5d93f27f83/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png',
-                    height: 200,
-                    width: 200),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Icon(
-                    Icons.favorite,
-                    color: Colors.grey,
-                    size: 30,
-                    shadows: [],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "A style icon gets some love from one of today's top trendsetters."
-                  "Pharell Williams put his spin on these shoes, which have all the clean,"
-                  "classic details of the beloved Stan Smith",
-                  style: TextStyle(color: Colors.grey),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "COLOR",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.left,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Image.network(
+                          'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1c2dfd0c-cd48-46c4-829a-4b5d93f27f83/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png',
+                          height: 200,
+                          width: 200),
+                      Positioned(
+                        bottom: 10,
+                        right: 20,
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.grey,
+                          size: 30,
+                          shadows: [],
                         ),
-                        Row(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Icon(
-                                Icons.circle,
-                                color: Colors.blue,
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white, elevation: 0),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Icon(
-                                Icons.circle,
-                                color: Colors.green,
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white, elevation: 0),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Icon(
-                                Icons.circle,
-                                color: Colors.yellow,
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white, elevation: 0),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Icon(
-                                Icons.circle,
-                                color: Colors.pink,
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white, elevation: 0),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Size",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          "10.21",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "ADD TO BAG+",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
                       ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white),
-                    ),
-                    Text(
-                      "\$95",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    )
-                  ],
-                )
-              ],
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "A style icon gets some love from one of today's top trendsetters."
+                    "Pharell Williams put his spin on these shoes, which have all the clean,"
+                    "classic details of the beloved Stan Smith",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "COLOR",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left,
+                          ),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.circle,
+                                  color: Colors.blue,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    elevation: 0),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.circle,
+                                  color: Colors.green,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    elevation: 0),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.circle,
+                                  color: Colors.yellow,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    elevation: 0),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.circle,
+                                  color: Colors.pink,
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    elevation: 0),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Size",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "10.21",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
             )),
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "ADD TO BAG+",
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+          ),
+          Text(
+            "\$95",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          )
+        ],
       ),
     );
   }
